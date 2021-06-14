343b3a38977f03fdae42d32356bc1a33905b3e09
Martin Raphael <martin.raph@live.fr>
2021-06-14 15:44:44 +0200

    buttom + css

--- views/index.php
+++ views/index.php
@@ -3,12 +3,16 @@
 require_once "header.php";
 require_once "default.php";
 require_once "main.php";
-require_once "sideMenu.php";
-require_once "footer.php";
-
+?>
+<div class="container">
+<?php
+    require_once "sideMenu.php";
+    require_once "productsTableView.php";
+?>
+</div>
+<?php
 require_once "statistiques.php";
 require_once "usersTableView.php";
-require_once "productsTableView.php";
-require_once "fromAddUser.php";
-
+require_once "footer.php";
+require_once "formAddUser.php";
 ?>
--- views/productsTableView.php
+++ views/productsTableView.php
@@ -1,54 +1,54 @@
 <?php
 // Connexion BDD
-require "controller/connect.php";
+require "C:\Users\Raphael\Desktop\CDA_Formation\php\GESTOCK\TpGestock_Pouet\controller\connect.php";
 
 
 $req = "SELECT * FROM produits";
 $resultat = $mysqli->query($req);
 
 ?>
+<div class="container">
+  <table 
+      id="table"
+      class="table table-striped">
+    <thead class="thead-dark">
+      <tr>
+        <th scope="col">Id</th>
+        <th scope="col">Code produit</th>
+        <th scope="col">Nom</th>
+        <th scope="col">Detail</th>
+        <th scope="col">Catégorie</th>
+        <th scope="col">Quantité</th>
+        <th scope="col">Prix d'achat</th>
+        <th scope="col">Date d'expiration</th>
+        <th scope="col">Actif</th>
+        <th scope="col">Editer / Supprimer</th>
+      </tr>
+    </thead>
 
-<table 
-    id="table"
-    class="table table-striped">
-  <thead class="thead-dark">
-    <tr>
-      <th scope="col">Id</th>
-      <th scope="col">Code produit</th>
-      <th scope="col">Nom</th>
-      <th scope="col">Detail</th>
-      <th scope="col">Catégorie</th>
-      <th scope="col">Quantité</th>
-      <th scope="col">Prix d'achat</th>
-      <th scope="col">Date d'expiration</th>
-      <th scope="col">Actif</th>
-      <th scope="col">Editer / Supprimer</th>
-    </tr>
-  </thead>
-
-  <tbody>
-  <?php
-    while($champs = mysqli_fetch_assoc($resultat))
-    {
-        ?>
-        <tr>
-            <?php
-            echo "<th scope='row'>".$champs["id"]."</th>";
-                echo "<td>".$champs["code_produit"]."</td>";
-                echo "<td>".$champs["nom"]."</td>";
-                echo "<td>".$champs["detail"]."</td>";
-                echo "<td>".$champs["categorie"]."</td>";
-                echo "<td>".$champs["quantite"]."</td>";
-                echo "<td>".$champs["prix_achat"]."</td>";
-                echo "<td>".$champs["date_expiration"]."</td>";
-                echo "<td>".$champs["actif"]."</td>";
-                echo "<td></td>";
-            ?>
-        </tr>
-        <?php
-    }
-    ?>
-  </tbody>
-</table>
-
+    <tbody>
+    <?php
+      while($champs = mysqli_fetch_assoc($resultat))
+      {
+          ?>
+          <tr>
+              <?php
+              echo "<th scope='row'>".$champs["id"]."</th>";
+                  echo "<td>".$champs["code_produit"]."</td>";
+                  echo "<td>".$champs["nom"]."</td>";
+                  echo "<td>".$champs["detail"]."</td>";
+                  echo "<td>".$champs["categorie"]."</td>";
+                  echo "<td>".$champs["quantite"]."</td>";
+                  echo "<td>".$champs["prix_achat"]."</td>";
+                  echo "<td>".$champs["date_expiration"]."</td>";
+                  echo "<td>".$champs["actif"]."</td>";
+                  echo "<td></td>";
+              ?>
+          </tr>
+          <?php
+      }
+      ?>
+    </tbody>
+  </table>
+</div>
 
