f01822e7c02fba8fb94b311a03e4c69a09562ce9
Martin Raphael <martin.raph@live.fr>
2021-06-15 17:05:38 +0200

    css add

--- views/formAddClient.php
+++ views/formAddClient.php
@@ -2,22 +2,33 @@
     require_once "header.php";
 ?>
 <body>
-    <h1>AJOUTER UN CLIENT</h1>
-    <form action="../controller/clientRegistration.php" method="post">   
-        <div class="form-group">
-            <label for="nomInput">Nom</label>
-            <input type="text" class="form-control" id="nomInput" name="nomInput" required>
-            </select>
+    <div class="container-fluid">
+        <div class="row">
+            <div class="col-4">
+            <?php
+                require_once "sideMenu.php"
+            ?>
+            </div>
+            <div class="col-8">
+                <h1>AJOUTER UN CLIENT</h1>
+                <form action="../controller/clientRegistration.php" method="post">   
+                    <div class="form-group">
+                        <label for="nomInput">Nom</label>
+                        <input type="text" class="form-control" id="nomInput" name="nomInput" required>
+                        </select>
+                    </div>
+                    <div class="form-group">
+                        <label for="prenomInput">Prénom</label>
+                        <input type="text" class="form-control" id="prenomInput" name="prenomInput" required>
+                    </div>
+                    <div class="form-group">
+                        <label for="mailInput">Adresse mail</label>
+                        <input type="mail" class="form-control" id="mailInput" placeholder="exemple@exemple.com" name="mailInput" required>
+                        </select>
+                    </div>
+                    <button type="submit" class="btn btn-primary">Enregistrer</button>
+                </form>
+            </div>
         </div>
-        <div class="form-group">
-            <label for="prenomInput">Prénom</label>
-            <input type="text" class="form-control" id="prenomInput" name="prenomInput" required>
-        </div>
-        <div class="form-group">
-            <label for="mailInput">Adresse mail</label>
-            <input type="mail" class="form-control" id="mailInput" placeholder="exemple@exemple.com" name="mailInput" required>
-            </select>
-        </div>
-        <button type="submit" class="btn btn-primary">Enregistrer</button>
-    </form>
+    </div>
 </body>
