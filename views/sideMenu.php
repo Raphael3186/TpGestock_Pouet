5279d01715c322d74e4f6cff8fcd7ab279b4bcf9
MTeycheney <maxime.teycheney@gmail.com>
2021-06-15 12:02:33 +0200

    modif tableau

--- views/sideMenu.php
+++ views/sideMenu.php
@@ -30,17 +30,19 @@
                     </svg>
                 </i>
                 <br>
-            Users
+                Users
             </button>
-            <button type="button" class="btn btn-danger btn">
-            <i class="bi bi-graph-up">
-                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-graph-up" viewBox="0 0 16 16">
-                    <path fill-rule="evenodd" d="M0 0h1v15h15v1H0V0zm10 3.5a.5.5 0 0 1 .5-.5h4a.5.5 0 0 1 .5.5v4a.5.5 0 0 1-1 0V4.9l-3.613 4.417a.5.5 0 0 1-.74.037L7.06 6.767l-3.656 5.027a.5.5 0 0 1-.808-.588l4-5.5a.5.5 0 0 1 .758-.06l2.609 2.61L13.445 4H10.5a.5.5 0 0 1-.5-.5z"/>
-                </svg>
-            </i>
-            <br>
-            Statistiques
-        </button>
+            <a href="views/formAddUser.php">
+                <button type="button" class="btn btn-danger btn">
+                    <i class="bi bi-person-fill">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" fill="currentColor" class="bi bi-person-fill" viewBox="0 0 16 16">
+                                <path d="M3 14s-1 0-1-1 1-4 6-4 6 3 6 4-1 1-1 1H3zm5-6a3 3 0 1 0 0-6 3 3 0 0 0 0 6z"/>
+                        </svg>
+                    </i>
+                    <br>Enregistrer
+                    <br>Utilisateur
+               </button>
+            </a>
         </div>
     </div>
     <div class=" row ">
